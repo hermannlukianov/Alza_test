1c8e3a219aa856cbb27864e821b0498b552f48db
hermantester <herman.lukianov@tesena.com>
2024-05-30 10:09:04 +0200

    first commit

---
+++ test/smoke.spec.js
@@ -0,0 +1,14 @@
+import welcomeScreen from "../screenObjects/WelcomeScreenObject";
+import mainScreen from "../screenObjects/MainScreenObject";
+import Gestures from "../hepers/Gestures";
+
+describe("Main Screen functionality", () => {
+  
+  it("Gestures: scroll, swipe", async () => {
+    
+  });
+
+});
+
+
+//   adb uninstall io.appium.uiautomator2.server
